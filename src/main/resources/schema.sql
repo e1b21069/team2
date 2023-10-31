6b1f0d88722e1591809f179e289f68ce5e748a74
B21098 <e1b21098@oit.ac.jp>
2023-10-31 16:57:18 +0900

    リファクタリング

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,8 @@
-/* userやgroupといった名前はSQLでは予約語で使えないため，userNameとしていることに注意 */
-CREATE TABLE chamber (
-    id IDENTITY,
-    userName VARCHAR NOT NULL,
-    chamberName VARCHAR NOT NULL
+CREATE TABLE users (
+    id IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL
 );
-CREATE TABLE userinfo (
-    userName VARCHAR NOT NULL PRIMARY KEY,
-    age INT,
-    height DOUBLE NOT NULL
-);
-CREATE TABLE fruit (
-    id IDENTITY,
-    name VARCHAR NOT NULL,
-    price INT NOT NULL
+CREATE TABLE words (
+    id IDENTITY PRIMARY KEY,
+    word VARCHAR NOT NULL
 );
