35a2d4347ffea51097fa4018305178c18071f82d
e1b21069 <e1b21069@oit.ac.jp>
2023-11-14 14:59:31 +0900

    wordLodの仮実装

---
+++ src/main/java/oit/is/work/team2/model/WordLogMapper.java
@@ -0,0 +1,16 @@
+/*
+package oit.is.work.team2.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface WordLogMapper {
+    
+}
+*/
