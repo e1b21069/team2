5b9b9dc4d472c35ba03e7fed842de5f406d6c3b3
B21098 <e1b21098@oit.ac.jp>
2023-11-29 09:16:43 +0900

    部屋管理ページの追加

--- src/main/java/oit/is/work/team2/controller/AdminRoomsController.java
+++ src/main/java/oit/is/work/team2/controller/AdminRoomsController.java
@@ -1,5 +1,104 @@
 package oit.is.work.team2.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import oit.is.work.team2.model.RoomMapper;
+import oit.is.work.team2.service.AsyncAdminRoomsService;
+import oit.is.work.team2.model.Room;
+
+@Controller
+@RequestMapping("/room")
 public class AdminRoomsController {
-    
+
+    @Autowired
+    RoomMapper roomMapper;
+
+    @Autowired
+    AsyncAdminRoomsService adminRoomsService;
+
+    @GetMapping("")
+    public String room(ModelMap model) {
+        // 単語リストを取得
+        final ArrayList<Room> rooms = adminRoomsService.syncShowRoomsList();
+        model.addAttribute("rooms", rooms);
+        return "adminRooms.html";
+    }
+
+    @GetMapping("getList")
+    public String getRooms(ModelMap model) {
+        // 単語リストを取得
+        final ArrayList<Room> rooms = adminRoomsService.syncShowRoomsList();
+        model.addAttribute("rooms", rooms);
+        return "adminRooms.html";
+    }
+
+    @GetMapping("sse")
+    public SseEmitter sse() {
+        final SseEmitter sseEmitter = new SseEmitter();
+        this.adminRoomsService.asyncShowRoomsList(sseEmitter);
+        return sseEmitter;
+    }
+
+    @GetMapping("deleteRoom")
+    @Transactional
+    public String deleteRoom(@RequestParam Integer id, ModelMap model) {
+        // 選択した単語を削除し，削除対象の単語をmodelに登録
+        final Room room1 = this.adminRoomsService.syncDeleteRooms(id);
+        model.addAttribute("room1", room1);
+        // 残りの単語リストを取得してmodelに登録
+        final ArrayList<Room> rooms = adminRoomsService.syncShowRoomsList();
+        model.addAttribute("rooms", rooms);
+        return "adminRooms.html";
+    }
+
+    @GetMapping("editRoom")
+    @Transactional
+    public String editRoom(@RequestParam Integer id, ModelMap model) {
+        // 編集対象の単語を取得
+        final Room room2 = this.adminRoomsService.syncEditRooms(id);
+        model.addAttribute("room2", room2);
+        // 単語リストを取得
+        final ArrayList<Room> rooms = adminRoomsService.syncShowRoomsList();
+        model.addAttribute("rooms", rooms);
+        return "adminRooms.html";
+    }
+
+    @PostMapping("updateRoom")
+    public String updateRoom(@RequestParam Integer id, @RequestParam String word, ModelMap model) {
+        System.out.println("updateRoom");
+        System.out.println(id);
+        System.out.println(word);
+        Room room = new Room(id, word);
+        // 編集
+        this.adminRoomsService.syncUpdateRooms(room);
+        // 単語リストを取得
+        final ArrayList<Room> rooms = adminRoomsService.syncShowRoomsList();
+        model.addAttribute("rooms", rooms);
+        model.addAttribute("beforeRoom", word);
+        return "adminRooms.html";
+    }
+
+    @PostMapping("addRoom")
+    @Transactional
+    public String addRoom(ModelMap model, @RequestParam String word) {
+        model.addAttribute("addRoom", word);
+        // 単語を追加
+        this.adminRoomsService.syncAddRooms(word);
+        // 単語リストを取得
+        final ArrayList<Room> rooms = adminRoomsService.syncShowRoomsList();
+        model.addAttribute("rooms", rooms);
+        return "adminRooms.html";
+    }
+
 }
+
--- src/main/java/oit/is/work/team2/service/AsyncAdminRoomsService.java
+++ src/main/java/oit/is/work/team2/service/AsyncAdminRoomsService.java
@@ -1,5 +1,94 @@
 package oit.is.work.team2.service;
 
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import oit.is.work.team2.model.Room;
+import oit.is.work.team2.model.RoomMapper;
+
+@Service
 public class AsyncAdminRoomsService {
-    
+    private volatile boolean dbUpdated = false;
+
+    private final Logger logger = LoggerFactory.getLogger(AsyncAdminRoomsService.class);
+
+    @Autowired
+    RoomMapper roomMapper;
+
+    @Transactional
+    public Room syncDeleteRooms(int id) {
+        // 削除対象の単語を取得
+        Room Room = roomMapper.selectById(id);
+        // 削除
+        roomMapper.deleteById(id);
+        // 非同期でDB更新したことを共有する際に利用する
+        this.dbUpdated = true;
+        return Room;
+    }
+
+    @Transactional
+    public Room syncEditRooms(int id) {
+        // 編集対象の単語を取得
+        Room Room = roomMapper.selectById(id);
+        // 編集
+        roomMapper.updateById(Room);
+        // 非同期でDB更新したことを共有する際に利用する
+        this.dbUpdated = true;
+        return Room;
+    }
+
+    @Transactional
+    public Room syncUpdateRooms(Room Room) {
+        // 編集対象の単語を取得
+        roomMapper.updateById(Room);
+        // 非同期でDB更新したことを共有する際に利用する
+        this.dbUpdated = true;
+        return Room;
+    }
+
+    @Transactional
+    public void syncAddRooms(String word) {
+        // 追加
+        roomMapper.insert(word);
+        // 非同期でDB更新したことを共有する際に利用する
+        this.dbUpdated = true;
+    }
+
+    public ArrayList<Room> syncShowRoomsList() {
+        return roomMapper.selectAll();
+    }
+
+    @Async
+    public void asyncShowRoomsList(SseEmitter emitter) {
+        dbUpdated = true;
+        try {
+            while (true) {// 無限ループ
+                // DBが更新されていなければ0.5s休み
+                if (false == dbUpdated) {
+                    TimeUnit.MILLISECONDS.sleep(500);
+                    continue;
+                }
+                // DBが更新されていれば更新後の単語リストを取得してsendし，1s休み，dbUpdatedをfalseにする
+                ArrayList<Room> rooms = this.syncShowRoomsList();
+                emitter.send(rooms);
+                TimeUnit.MILLISECONDS.sleep(1000);
+                dbUpdated = false;
+            }
+        } catch (Exception e) {
+            // 例外の名前とメッセージだけ表示する
+            logger.warn("Exception:" + e.getClass().getName() + ":" + e.getMessage());
+        } finally {
+            emitter.complete();
+        }
+        System.out.println("asyncShowRoomsList complete");
+    }
+
 }
