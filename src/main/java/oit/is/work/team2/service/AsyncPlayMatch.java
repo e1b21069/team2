37c18db0acf886dacc54c2012e0b233d98cb94b5
m4roon <114385848+B21098@users.noreply.github.com>
2023-12-26 18:13:03 +0900

    Revert "先攻が2回問題の解消"

--- src/main/java/oit/is/work/team2/service/AsyncPlayMatch.java
+++ src/main/java/oit/is/work/team2/service/AsyncPlayMatch.java
@@ -30,7 +30,6 @@ public class AsyncPlayMatch {
 
   private volatile boolean dbUpdated = false;
   private volatile boolean wdbUpdated = false;
-  private volatile boolean secondUpdated = false;
   private final Logger logger = LoggerFactory.getLogger(AsyncPlayMatch.class);
 
   @Autowired
@@ -68,20 +67,11 @@ public class AsyncPlayMatch {
 
   @Async
   public void asyncShowWordLogsList(SseEmitter emitter) {
-    int count = 0;
+    // dbUpdated = true; これをオンにするとリアルタイム対戦のようになる
     try {
       while (true) {// 無限ループ
         // DBが更新されていなければ0.1s休み
         if (false == dbUpdated) {
-          if(count % 100 == 0) {
-            Map<String, String> data = new HashMap<>();
-            data.put("type", "msg");
-            data.put("message", "dontMove");
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonData = objectMapper.writeValueAsString(data);
-            emitter.send(jsonData);
-          }
-          count++;
           TimeUnit.MILLISECONDS.sleep(100);
           continue;
         }
@@ -107,7 +97,6 @@ public class AsyncPlayMatch {
           emitter.send(jsonData);
         }
         dbUpdated = false;
-        secondUpdated = true;
       }
     } catch (Exception e) {
       // 例外の名前とメッセージだけ表示する
@@ -127,70 +116,6 @@ public class AsyncPlayMatch {
         emitter.send(wordLogs);
         wdbUpdated = false;
         TimeUnit.MILLISECONDS.sleep(300);
-      }
-    } catch (Exception e) {
-      // 例外の名前とメッセージだけ表示する
-      logger.warn("Exception:" + e.getClass().getName() + ":" + e.getMessage());
-    } finally {
-      emitter.complete();
-    }
-    System.out.println("asyncShowDictionariesList complete");
-  }
-
-  @Async
-  public void asyncShowSecond(SseEmitter emitter) {
-    int count = 0;
-    try {
-      while (true) {// 無限ループ
-        // DBが更新されていなければ0.5s休み
-        if (false == secondUpdated) {
-          if(count % 100 == 0) {
-            Map<String, String> data = new HashMap<>();
-            data.put("type", "msg");
-            data.put("message", "dontMove");
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonData = objectMapper.writeValueAsString(data);
-            emitter.send(jsonData);
-          }
-          count++;
-          TimeUnit.MILLISECONDS.sleep(100);
-          continue;
-        }
-        if (false == dbUpdated) {
-          if(count % 100 == 0) {
-            Map<String, String> data = new HashMap<>();
-            data.put("type", "msg");
-            data.put("message", "dontMove");
-            ObjectMapper objectMapper = new ObjectMapper();
-            String jsonData = objectMapper.writeValueAsString(data);
-            emitter.send(jsonData);
-          }
-          count++;
-          TimeUnit.MILLISECONDS.sleep(100);
-          continue;
-        }
-        // DBが更新されていれば更新後の単語リストを取得してsendし，1s休み，dbUpdatedをfalseにする
-        // emitter.send(true);
-        String ans = wordLogMapper.selectAns();
-        String word = matchMapper.selectWord(1);
-
-        if(ans.equals(word)) {
-          Map<String, String> data = new HashMap<>();
-          data.put("type", "msg");
-          data.put("message", "loserScreen");
-          ObjectMapper objectMapper = new ObjectMapper();
-          String jsonData = objectMapper.writeValueAsString(data);
-          emitter.send(jsonData);
-        } else {
-          Map<String, String> data = new HashMap<>();
-          data.put("type", "msg");
-          data.put("message", "nextScreen");
-          // Jackson ObjectMapperを使用してJSON形式の文字列に変換
-          ObjectMapper objectMapper = new ObjectMapper();
-          String jsonData = objectMapper.writeValueAsString(data);
-          emitter.send(jsonData);
-        }
-        dbUpdated = false;
       }
     } catch (Exception e) {
       // 例外の名前とメッセージだけ表示する
