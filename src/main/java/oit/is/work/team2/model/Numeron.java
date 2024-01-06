0a293b42ea9d43c9f7c45b7f396ce74f1238fb22
e1b21064 <e1b21064@oit.ac.jp>
2024-01-06 18:55:18 +0900

    biteのロジックを改善

--- src/main/java/oit/is/work/team2/model/Numeron.java
+++ src/main/java/oit/is/work/team2/model/Numeron.java
@@ -23,21 +23,73 @@ public class Numeron {
   }
 
   // wordの文字列の中にansの文字列が何個含まれているかどうか
+  /*
+   * public int bitejudge(String word, String ans) {
+   * int bitecnt = 0;
+   * for (int i = 0; i < ans.length(); i++) {
+   * for (int j = 0; j < word.length(); j++) {
+   * if (ans.charAt(i) == word.charAt(j) && i != j) {
+   * bitecnt++;
+   * }
+   * }
+   * }
+   * // お題「rose」答え「rooo」=> 2eat 4bite -> 2eat 0bite表示
+   * bitecnt = bitecnt - eatcnt;
+   * if (bitecnt < 0) {
+   * bitecnt = 0;
+   * }
+   * return bitecnt;
+   * }
+   */
+
   public int bitejudge(String word, String ans) {
     int bitecnt = 0;
+    int flag0 = 0, flag1 = 0, flag2 = 0, flag3 = 0;
+    int finishFlag = 0;
     for (int i = 0; i < ans.length(); i++) {
       for (int j = 0; j < word.length(); j++) {
-        if (ans.charAt(i) == word.charAt(j) && i != j) {
-          bitecnt++;
+        if (word.charAt(i) == ans.charAt(j) && i != j) {
+          switch (j) {
+            case 0:
+              if (flag0 == 0) {
+                bitecnt++;
+                flag0 = 1;
+                finishFlag = 1;
+              }
+              break;
+
+            case 1:
+              if (flag1 == 0) {
+                bitecnt++;
+                flag1 = 1;
+                finishFlag = 1;
+              }
+              break;
+
+            case 2:
+              if (flag2 == 0) {
+                bitecnt++;
+                flag2 = 1;
+                finishFlag = 1;
+              }
+              break;
+
+            case 3:
+              if (flag3 == 0) {
+                bitecnt++;
+                flag3 = 1;
+                finishFlag = 1;
+              }
+              break;
+          }
+        }
+        if (finishFlag == 1) {
+          finishFlag = 0;
+          break;
         }
       }
     }
-    // お題「rose」答え「rooo」=> 2eat 4bite -> 2eat 0bite表示
-    bitecnt = bitecnt - eatcnt;
-    if (bitecnt < 0) {
-      bitecnt = 0;
-    }
-    return bitecnt;
+    return bitecnt - eatjudge(word, ans);
   }
 
 }
